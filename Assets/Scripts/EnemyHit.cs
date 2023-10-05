c0f6d53a524508f2d2e09700a90cf75b84eb43a6
UESTC-xdd <821378157@qq.com>
2023-10-05 13:38:02 -0400

    Hit FeedBack

--- Assets/Scripts/EnemyHit.cs
+++ Assets/Scripts/EnemyHit.cs
@@ -6,8 +6,21 @@ public class EnemyHit : MonoBehaviour
 {
     public int FullHP;
 
+    public bool ShowHitFeedback;
+    public Material RedMat;
+    public float RedTime;
+
+    [SerializeField]
+    private MeshRenderer m_Renderer;
+    private Material PreMat;
+
     public int CurHp { get; set; }
     public bool IsDead { get; set; }
+
+    private void Awake()
+    {
+        PreMat = m_Renderer.sharedMaterial;
+    }
 
     private void OnEnable()
     {
@@ -20,6 +33,13 @@ public class EnemyHit : MonoBehaviour
         {
             Debug.Log("Receive Damage");
             CurHp -= damage;
+
+            if(ShowHitFeedback)
+            {
+                StopCoroutine(HitFeedbackCou());
+                StartCoroutine(HitFeedbackCou());
+            }
+
             if(CurHp<=0)
             {
                 EnemyDie();
@@ -32,5 +52,12 @@ public class EnemyHit : MonoBehaviour
     {
         IsDead= true;
         Debug.Log("EnemyDie");
+    }
+
+    public IEnumerator HitFeedbackCou()
+    {
+        m_Renderer.sharedMaterial = RedMat;
+        yield return new WaitForSeconds(RedTime);
+        m_Renderer.sharedMaterial = PreMat;
     }
 }
