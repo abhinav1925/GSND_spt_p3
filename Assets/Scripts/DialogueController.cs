22011d1f474c870140730e42d150e67e48f14c24
Zekai Gong <59636500+KevinGzk@users.noreply.github.com>
2023-10-05 16:20:03 -0400

    Opening dialogue done

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public AudioSource audioSource;
+    public GameObject telephoneUI;
+
+    [System.Serializable]
+    public class DialogueLine
+    {
+        public string name;
+        public string sentence;
+        public AudioClip audioClip;
+    }
+
+    public DialogueLine[] lines;
+    private Queue<DialogueLine> dialogueQueue;
+    private bool dialogueStarted = false;
+
+    void Start()
+    {
+        dialogueQueue = new Queue<DialogueLine>(lines);
+        StartDialogue();
+    }
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Space)) && dialogueStarted)
+        {
+            DisplayNextSentence();
+        }
+    }
+
+    public void StartDialogue()
+    {
+        dialogueStarted = true;
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (dialogueQueue.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        DialogueLine line = dialogueQueue.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(line));
+    }
+
+    IEnumerator TypeSentence(DialogueLine line)
+    {
+        nameText.text = line.name;
+        dialogueText.text = "";
+        audioSource.clip = line.audioClip;
+        audioSource.Play();
+        foreach (char letter in line.sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        dialogueStarted = false;
+        StartCoroutine(WaitForCloseCommand());
+    }
+    IEnumerator WaitForCloseCommand()
+    {
+        while(!Input.GetKeyDown(KeyCode.E))
+        {
+            yield return null;
+        }
+        telephoneUI.SetActive(false);
+    }
+}
